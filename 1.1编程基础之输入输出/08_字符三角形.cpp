3b0fe71e6af523c3c80353afc928a5bcd3017034
不是肥狗 <49602249+Jarvisgame@users.noreply.github.com>
2020-08-21 16:59:52 +0800

    Add files via upload

---
+++ 1.1编程基础之输入输出/08_字符三角形.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char d;
+	scanf("%c",&d);
+	printf("  %c\n", d);
+	printf(" %c%c%c\n", d, d, d);
+	printf("%c%c%c%c%c\n", d, d, d, d, d);
+	return 0;
+}
---
+++ 1.1编程基础之输入输出/09_字符菱形.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	char d;
+	scanf("%c", &d);
+	printf("  %c\n", d);
+	printf(" %c%c%c\n", d, d, d);
+	printf("%c%c%c%c%c\n", d, d, d, d, d);
+	printf(" %c%c%c\n", d, d, d);
+	printf("  %c\n", d);
+	return 0;
+}
